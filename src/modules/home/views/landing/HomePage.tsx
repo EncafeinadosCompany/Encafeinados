d52360a9401c4ae6b859d0a250ece4967975d63d
Crony <lopezcrony@gmail.com>
2025-03-28 14:50:19 -0500

    feat: :sparkles: update HomePage and TeamSection components - Reintroduce TeamSection in HomePage - Revise AboutSection text for clarity - Update team members' details in TeamSection

--- src/modules/home/views/landing/HomePage.tsx
+++ src/modules/home/views/landing/HomePage.tsx
@@ -1,8 +1,8 @@
 import { HeroSection } from '@/common/widgets/home/HeroSection';
 import { StoreCarousel } from '@/common/widgets/home/StoreCarousel';
-// import { TeamSection } from '@/common/widgets/TeamSection';
+import { TeamSection } from '@/common/widgets/home/TeamSection';
 import { BenefitsSection } from '@/common/widgets/home/BenefitsSection';
-import { AboutSection } from '@/common/widgets/home/AboutSection';
+
 import { Footer } from '@/common/widgets/home/Footer';
 import { MapTeaser } from '@/common/molecules/home/MapTeaser';
 
@@ -16,7 +16,7 @@ const HomePage = () => {
         
         <StoreCarousel />
         <BenefitsSection />
-        {/* <TeamSection /> */}
+        <TeamSection />
         {/* <AboutSection /> */}
         <Footer />
         
