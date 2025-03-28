c3c948e636a839ca77e537afa64be40463c1cf67
Santiago Herrera <santiherreravelas@gmail.com>
2025-03-28 15:19:14 -0500

    refactor: comments out

--- jest.config.ts
+++ jest.config.ts
@@ -1,45 +1,37 @@
-import { defaults } from 'jest-config';
-import path from 'path';
+import { defaults } from "jest-config";
+import path from "path";
 export default {
-
   rootDir: path.resolve(__dirname),
-  // Extiende las configuraciones predeterminadas de Jest
   ...defaults,
-
-  // Especifica el entorno de pruebas (jsdom para pruebas de frontend)
-  testEnvironment: 'jest-environment-jsdom',
-
-  // Archivos que se ejecutan antes de cada suite de pruebas
-  setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
-
-  // Mapeo de alias para importaciones (si usas alias como @/)
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
 
   // Transforma archivos TypeScript y JavaScript usando Babel
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
 
   // Patrones para buscar archivos de prueba
   testMatch: [
-    '<rootDir>/src/tests/unit/**/*.test.(js|jsx|ts|tsx)', // Pruebas unitarias
+    "<rootDir>/src/tests/unit/**/*.test.(js|jsx|ts|tsx)", // Pruebas unitarias
   ],
 
   // Ignorar estas carpetas al buscar pruebas
-  testPathIgnorePatterns: ['<rootDir>/node_modules/'], // Ignora node_modules y pruebas E2E
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"], // Ignora node_modules y pruebas E2E
 
   // Extensiones de archivo que Jest reconocerá
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
 
   // Habilitar salida detallada
   verbose: true,
 
   // Configuración adicional para TypeScript
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json', // Usa tu archivo tsconfig
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.json", // Usa tu archivo tsconfig
     },
   },
 };
