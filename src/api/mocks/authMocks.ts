ed5d16d456c61a2903bfe2f1127177cf230efe92
valentina-RGB <valensc2545@gmail.com>
2025-03-31 16:53:43 -0500

    feat: :bug: refactor: update file paths, mock data, and component exports  - Renamed `customIcon.ts` to `customIcon.tsx` and updated its content - Modified `authMocks.ts` to remove unnecessary fields and add a password field - Updated import paths for `LinkReturn` and `LoadingSpinner` components - Added new icons `customIcon.tsx` and `currentLocationIcon.tsx` - Refactored `LoadingSpinner` to use default export - Adjusted `useMapData` hook to fix store data access - Cleaned up unnecessary imports and whitespace in multiple files

--- src/api/mocks/authMocks.ts
+++ src/api/mocks/authMocks.ts
@@ -2,8 +2,6 @@
 import { User } from '../types/authTypes'
 
 export const mockUser: User = {
-  id: '1',
-  name: 'John Doe',
   email: 'john@example.com',
-  role: 'admin',
+  password: 'admin',
 }
