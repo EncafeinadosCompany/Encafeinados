8b3728136d909f0b9d8a3d524836372e82fada54
Santiago Herrera <santiherreravelas@gmail.com>
2025-03-31 20:13:16 -0500

    feat: setting coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,44 +1,35 @@
 import { defaults } from "jest-config";
 import path from "path";
+
 export default {
   rootDir: path.resolve(__dirname),
-  // Extiende las configuraciones predeterminadas de Jest
   ...defaults,
 
-  // Especifica el entorno de pruebas (jsdom para pruebas de frontend)
   testEnvironment: "jest-environment-jsdom",
-
-  // Archivos que se ejecutan antes de cada suite de pruebas
   setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
-
-  // Mapeo de alias para importaciones (si usas alias como @/)
   moduleNameMapper: {
-      "^@/(.*)$": "<rootDir>/src/$1"
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
-
-  // Transforma archivos TypeScript y JavaScript usando Babel
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
-
-  // Patrones para buscar archivos de prueba
-  testMatch: [
-    "<rootDir>/tests/unit/**/*.test.(js|jsx|ts|tsx)", // Pruebas unitarias
-  ],
-
-  // Ignorar estas carpetas al buscar pruebas
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"], // Ignora node_modules y pruebas E2E
-
-  // Extensiones de archivo que Jest reconocerá
+  testMatch: ["<rootDir>/tests/unit/**/*.test.(js|jsx|ts|tsx)"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"],
-
-  // Habilitar salida detallada
   verbose: true,
-
-  // Configuración adicional para TypeScript
   globals: {
     "ts-jest": {
-      tsconfig: "<rootDir>/tsconfig.json", // Usa tu archivo tsconfig
+      tsconfig: "<rootDir>/tsconfig.json",
+    },
+  },
+
+  // Cobertura mínima requerida
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
 };
