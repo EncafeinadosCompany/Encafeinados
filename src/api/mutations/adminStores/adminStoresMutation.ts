62d18ab2bcb02cfe5561ca01de599f04bfd7e27a
valentina-RGB <valensc2545@gmail.com>
2025-04-11 12:44:35 -0500

    feat: :zap: add login mutation after successful admin store registration

--- src/api/mutations/adminStores/adminStoresMutation.ts
+++ src/api/mutations/adminStores/adminStoresMutation.ts
@@ -3,11 +3,13 @@ import { RegisterAdminStores } from "@/api/types/adminStoresTypes";
 import { useError } from "@/common/hooks/auth/useErrors";
 import { handleApiError } from "@/common/utils/errors/handleApiError";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useLoginMutation } from "../auth/authMutations";
 
 const authClient = new AuthClient();
 
 export const useAdminStoreMutation = () => {
     const queryClient = useQueryClient()
+    const useLonginMutation = useLoginMutation()
     const useErrors = useError("registerAdminStores")
   
     return useMutation<any, Error, RegisterAdminStores>({
@@ -21,8 +23,12 @@ export const useAdminStoreMutation = () => {
           throw handleApiError(error)
         }
       },
-      onSuccess: (data) => {
+      onSuccess: (data, variable) => {
         queryClient.invalidateQueries({ queryKey: ['adminStore'] });
+        useLonginMutation.mutate({
+          email: variable.userData.email,
+          password: variable.userData.password
+        })
       },
       onError: (error: any) => {
         useErrors(error);
