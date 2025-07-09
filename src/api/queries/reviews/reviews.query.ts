fbee6da2264c4d549fad3b7525d0b7f86131c89c
Santiago Florez <santiagoflorezv23@gmail.com>
2025-07-09 15:01:25 -0500

    fix: include branch ID in query key for useReviewsByIdBranches hook

--- src/api/queries/reviews/reviews.query.ts
+++ src/api/queries/reviews/reviews.query.ts
@@ -6,7 +6,7 @@ const authClient = new AuthClient()
 
 export const useReviewsByIdBranches = (id:number) => {
   return useQuery<SingleReview[]>({
-    queryKey: ['reviews'],
+    queryKey: ['reviews', id], // Include the branch ID in the query key
     queryFn: async () => {
       const response = await authClient.get<Reviews>(`/reviews/branch/${id}`)
       return response.reviews
