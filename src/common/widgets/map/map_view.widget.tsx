d86df156a722037bdded299ec09b2b0349a2f9c7
Santiago Florez <santiagoflorezv23@gmail.com>
2025-07-09 15:07:14 -0500

    refactor: remove commented-out code for active cafe marker animation in MapView component

--- src/common/widgets/map/map_view.widget.tsx
+++ src/common/widgets/map/map_view.widget.tsx
@@ -651,7 +651,6 @@ const MapView: React.FC<MapViewProps> = ({ view: showView }) => {
         )}
       </AnimatePresence>
       
-      {/* Active cafe marker animation */}
       {activeCafe && (
         <motion.div
           className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 pointer-events-none"
