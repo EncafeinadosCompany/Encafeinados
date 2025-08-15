8bf870a3014dc86488cfe2103992e8e31d61a9fb
Santiago Herrera Velásquez <57156413+santiagoHV@users.noreply.github.com>
2025-08-15 16:53:23 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import istanbul from "vite-plugin-istanbul";
 
+const port = Number(process.env.PORT || 4173)
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
